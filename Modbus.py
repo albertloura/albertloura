57124daa83ef72823302de52434e652575238b6a
Albert D. Loura <albertloura@gmail.com>
2022-03-24 21:21:52 -0300

    alteração 2

--- Modbus.py
+++ Modbus.py
@@ -7,16 +7,17 @@ import os #Biblioteca para limpar a tela
 client=ModbusClient(host="192.168.0.7", port=8899)
 try:
     print('Client starting...')
-    client.unit_id(10)
+    client.unit_id(0)
     client.open()
     print('Client is online.')
-    sleep(2)
     while True:
         os.system('cls') or None
-        dados = list(client.read_holding_registers(0,50))
-        print(f'Potência consumida: {dados[1]/100}W')
-        print(f'Tensão elétrica: {dados[22]/10}V')
-        print(f'Corrente elétrica: {dados[25]/100}A')
+        pot=client.read_holding_registers(1,1)
+        ten=client.read_holding_registers(22,1)
+        cor=client.read_holding_registers(25,1)
+        print(f'Potência consumida: {pot[0]/100}W')
+        print(f'Tensão elétrica: {ten[0]/10}V')
+        print(f'Corrente elétrica: {cor[0]/100}A')
         sleep(1)
         Continue
 except:
